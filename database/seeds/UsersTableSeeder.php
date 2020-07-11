5b6374e2f2fd67390cefa716b95ec7af77df71d2
ariiyantto168 <ariiyantto168@gmail.com>
2020-07-11 12:39:46 +0700

    updated

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,7 +24,7 @@ class UsersTableSeeder extends Seeder
             'updated_at' => date('Y-m-d H:i:s'),
           ],
           [
-            'name' => 'M Aries Budi jaya',
+            'name' => 'M Aries Budi Indra jaya',
             'email' => 'rakgroupserver@gmail.com',
             'role' => '1',
             'role_type' => 'LD',
